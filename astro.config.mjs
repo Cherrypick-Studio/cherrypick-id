2b141da1ced0b91cfdb89016f7356cefcd3cffd4
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2026-02-28 14:48:43 +0700

    chore: update site URL and enhance package.json metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cherrypick.id',
+	site: 'https://id.cherrypick.id',
 	integrations: [react(), tailwind(), sitemap()],
 });
