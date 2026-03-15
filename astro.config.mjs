b99e6bbf20bce9eca5bf307e7b03f3ec1e1a6047
Taufan Fadhilah <taufanfadhilahiskandar@gmail.com>
2026-03-15 13:49:25 +0700

    chore: update site URLs to reflect new domain for Cherrypick Studio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://id.cherrypick.id',
+	site: 'https://id.cherrypick.studio',
 	integrations: [react(), tailwind(), sitemap()],
 });
